719ded002afa0c0f71a6bf5eb8d87dd5e9438300
Mikaela Schultz <schultzm2024@fau.edu>
2025-01-16 13:09:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("My Streamlit App")
-st.write("Welcome to my first Streamlit app deployed on Streamlit Cloud!")
+st.write("Welcome to my first Streamlit app deployed on Streamlit Cloud! Lets goooooooo")
